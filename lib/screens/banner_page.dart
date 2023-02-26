af8cb3b83809ad2c34b589244c2dfec0a76460d6
GlitchAmbush <lemmytheleo@yahoo.com>
2023-02-26 15:47:11 -0500

    Added Banner Page. No Submenu to show rates

--- lib/screens/banner_page.dart
+++ lib/screens/banner_page.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
 
-class BannerPage extends StatelessWidget {
+class BannerPage extends StatefulWidget {
   const BannerPage({Key? key}) : super(key: key);
+
+  @override
+  State<BannerPage> createState() => _BannerPageState();
+}
+
+class _BannerPageState extends State<BannerPage> {
+  final ScrollController _firstController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.purple[300],
-          title: const Text('Banner'),
-        ),
-        body: const Center(
-          child: Text('Banner'),
-        ));
+        appBar:
+            AppBar(backgroundColor: Colors.green, title: const Text('Banners')),
+        body: ListView.builder(
+            controller: _firstController,
+            itemCount: 6,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size.fromHeight(150)),
+                    onPressed: () {},
+                    child: Text('Banner $index')),
+              );
+            }));
   }
 }
