b4616e87d5077204304e2bf306da537c041cf833
p3ndej <mmitesh888@gmail.com>
2023-02-26 17:14:34 -0500

    Achivements page with simple scrollable text eleme

--- lib/screens/achievements_page.dart
+++ lib/screens/achievements_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class AchievementsPage extends StatelessWidget {
+class AchievementsPage extends StatefulWidget {
   const AchievementsPage({Key? key}) : super(key: key);
 
+  @override
+  State<AchievementsPage> createState() => _AchievementsPageState();
+}
+
+class _AchievementsPageState extends State<AchievementsPage> {
+  final ScrollController _firstController = ScrollController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,8 +16,16 @@ class AchievementsPage extends StatelessWidget {
           backgroundColor: Colors.purple[300],
           title: const Text('Achievements'),
         ),
-        body: const Center(
-          child: Text('Achievements'),
-        ));
+        body: ListView.builder(
+            controller: _firstController,
+            itemCount: 15,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.purple)),
+                child: Center(child: Text('Achievement $index')),
+              );
+            }));
   }
 }
