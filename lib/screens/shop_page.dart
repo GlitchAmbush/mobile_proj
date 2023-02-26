7fb67927949c995dab1f7366fb0d52c77d1968cc
p3ndej <mmitesh888@gmail.com>
2023-02-26 16:37:37 -0500

    shop page has items

--- lib/screens/shop_page.dart
+++ lib/screens/shop_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ShopPage extends StatelessWidget {
+class ShopPage extends StatefulWidget {
   const ShopPage({Key? key}) : super(key: key);
 
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  final ScrollController _firstController = ScrollController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,8 +16,18 @@ class ShopPage extends StatelessWidget {
           backgroundColor: Colors.purple[300],
           title: const Text('Shop'),
         ),
-        body: const Center(
-          child: Text('Shop'),
-        ));
+        body: ListView.builder(
+            controller: _firstController,
+            itemCount: 15,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size.fromHeight(100)),
+                    onPressed: () {},
+                    child: Text('Upgrade $index')),
+              );
+            }));
   }
 }
