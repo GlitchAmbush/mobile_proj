b4cd801363872e85637d16ecccb2c5c489b520d4
Kym Gripal <kym.gripal@georgebrown.ca>
2023-03-21 15:50:45 -0400

    Added rates

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -17,57 +17,89 @@ Map<String, Map> bannerTitle = {
 
 // Hololive 1st Gen Members + Rates
 Map<String, double> firstGen = {
-  'Tokino Sora': 0.2,
-  'Roboco-san': 0.2,
-  'Sakura Miko': 0.2,
-  'Hoshimachi Suisei': 0.2,
-  'AZKi': 0.2,
+  'Tokino Sora': 0.01728732,
+  'Roboco-san': 0.29275859,
+  'Sakura Miko': 0.24694526,
+  'Hoshimachi Suisei': 0.03581443,
+  'AZKi': 0.4071944,
 };
 
 // 2nd Gen
 Map<String, double> secondGen = {
-  "Minato Aqua": 0.25,
-  "Murasaki Shion": 0.2,
-  "Oozora Subaru": 0.35,
-  "Yuzuki Choco": 0.1,
-  "Nakiri Ayame": 0.1,
+  "Minato Aqua": 0.00797492,
+  "Murasaki Shion": 0.18404691,
+  "Oozora Subaru": 0.10607377,
+  "Yuzuki Choco": 0.26874224,
+  "Nakiri Ayame": 0.43316215,
 };
 
 // 3rd Gen
 Map<String, double> thirdGen = {
-  "Usada Pekora": 0.35,
-  "Houshou Marine": 0.45,
-  "Shiranui Flare": 0.1,
-  "Shirogane Noel": 0.1,
+  "Usada Pekora": 0.0628053,
+  "Houshou Marine": 0.08567891,
+  "Shiranui Flare": 0.38840591,
+  "Shirogane Noel": 0.46310987,
 };
 
 // 4th Gen
 Map<String, double> fourthGen = {
-  "Amane Kanata": 0.3,
-  "Tsunomaki Watame": 0.4,
-  "Tokoyami Towa": 0.2,
-  "Himemori Luna": 0.1
+  "Amane Kanata": 0.09903611,
+  "Tsunomaki Watame": 0.07313679,
+  "Tokoyami Towa": 0.17234697,
+  "Himemori Luna": 0.65548012,
 };
 
 // 5th Gen
 Map<String, double> fifthGen = {
-  "Yukihana Lamy": 0.1,
-  "Momosuzu Nene": 0.4,
-  "Omaru Polka": 0.2,
-  "Shishiro Botan": 0.3,
+  "Yukihana Lamy": 0.4717614,
+  "Momosuzu Nene": 0.32602647,
+  "Omaru Polka": 0.04782652,
+  "Shishiro Botan": 0.15438561,
 };
 
 // HoloGamers
-Map<String, double> gamers = {'Test': 1};
+Map<String, double> gamers = {
+  "Shirakami Fubuki": 0.01706313,
+  "Nekomata Okayu": 0.02456546,
+  "Inugami Korone": 0.29439443,
+  "Ookami Mio": 0.66397698,
+};
 
 // HoloX
-Map<String, double> holoX = {'Test': 1};
+Map<String, double> holoX = {
+  "La+ Darknesss": 0.02830267,
+  "Takane Lui": 0.13455981,
+  "Hakui Koyori": 0.11580157,
+  "Sakamata Chloe": 0.04557772,
+  "Kazama Iroha": 0.67575822,
+};
 
 // HoloMyth
-Map<String, double> myth = {'Test': 1};
+Map<String, double> myth = {
+  "Mori Calliope": 0.27251266,
+  "Takanashi Kiara": 0.36888629,
+  "Ninomae Ina'nis": 0.19966919,
+  "Gawr Gura": 0.02556798,
+  "Watson Amelia": 0.13336388,
+};
 
 // HoloCouncil
-Map<String, double> council = {'Test': 1};
+Map<String, double> council = {
+  "Ceres Fauna": 0.01728732,
+  "Ouro Kronii": 0.29275859,
+  "Nanashi Mumei": 0.24694526,
+  "Hakos Baelz": 0.03581443,
+};
 
-// Holo ID
-Map<String, double> indonesia = {'Test': 1};
+// Holo ID (1-3)
+Map<String, double> indonesia = {
+  "Ayunda Risu": 0.03244145,
+  "Moona Hoshinova": 0.05248011,
+  "Airani Iofifteen": 0.12140367,
+  "Kureiji Ollie": 0.0147346,
+  "Anya Melfissa": 0.17570587,
+  "Pavolia Reine": 0.06470523,
+  "Vestia Zeta": 0.03615627,
+  "Kaela Kovalskia": 0.34465842,
+  "Kobo Kanaeru": 0.15771437,
+};
