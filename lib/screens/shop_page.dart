99486c7eaf22f67327675df7e55bd9dbbaffc59d
Kym Gripal <kym.gripal@georgebrown.ca>
2023-03-26 22:52:35 -0400

    Initial shop functionality

--- lib/screens/shop_page.dart
+++ lib/screens/shop_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../util/user_data.dart' as user;
+import 'dart:math';
 
 class ShopPage extends StatefulWidget {
   const ShopPage({Key? key}) : super(key: key);
@@ -16,18 +17,35 @@ class _ShopPageState extends State<ShopPage> {
     return Scaffold(
         appBar: AppBar(
             backgroundColor: Colors.blue,
-            title: Text('HoloCoins: ${user.holoCoins}')),
+            centerTitle: true,
+            title: Text(
+              'HoloCoins: ${user.holoCoins}',
+            )),
         body: ListView.builder(
             controller: _firstController,
-            itemCount: 15,
+            itemCount: 10,
             itemBuilder: (BuildContext context, int index) {
+              int random = Random().nextInt(max(1, 1000));
+              int random2 = Random().nextInt(max(1, 15));
               return Padding(
                 padding: const EdgeInsets.all(5.0),
                 child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
                         minimumSize: const Size.fromHeight(100)),
-                    onPressed: () {},
-                    child: Text('Upgrade $index \nIncrease Money :D')),
+                    onPressed: () {
+                      setState(() {
+                        if (user.holoCoins < index + 1) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Text('Not enough HoloCoins')));
+                        } else {
+                          user.holoCoins -= random2;
+                          user.holoCoins += random;
+                        }
+                      });
+                    },
+                    child:
+                        Text('Get $random HoloCoins for $random2 HoloCoins')),
               );
             }));
   }
